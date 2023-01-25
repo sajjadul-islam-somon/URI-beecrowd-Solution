830b9785ca3f7786549bcb30eb05489e75dc6bf8
Sajjadul Islam Shuvo <100591443+sajjadul-islam-shuvo@users.noreply.github.com>
2023-01-25 09:33:14 +0600

    Create 2581.cpp

---
+++ 2581.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int t;
+    string s;
+    cin>>t;
+    while (t--)
+    {
+        cin.ignore();
+        getline(cin,s);
+        cout<<"I am Toorg!\n";
+    }
+    return 0;
+}
