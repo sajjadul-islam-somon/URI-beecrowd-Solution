2c1973471025ac1c4fb19d2b782df77b4a3b5cf5
Sajjadul Islam Shuvo <100591443+sajjadul-islam-shuvo@users.noreply.github.com>
2023-01-25 20:25:33 +0600

    Create 2826.cpp

---
+++ 2826.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main()
+{
+    char s1[21],s2[21];
+    cin>>s1>>s2;
+    // int cmp = s1.compare(s2);  
+    int cmp = strcmp(s1,s2);    /*This function is faster than both compare() , lexicographical_compare() function.*/
+    if (cmp<0)
+    {
+        cout<<s1<<"\n"<<s2<<"\n";
+    }
+    else if (cmp>0)
+    {
+        cout<<s2<<"\n"<<s1<<"\n";
+    }
+    return 0;
+}
