a3d95a3731f53a49e4b76512e86c452fe6275dd1
Sajjadul Islam Shuvo <100591443+sajjadul-islam-shuvo@users.noreply.github.com>
2023-02-07 08:25:06 +0600

    Create 2355.cpp

---
+++ 2355.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+    unsigned long n;
+    while(cin >> n and n != 0){
+        double avg_g = n/90.0;
+        int bra,gar;
+        bra = floor(avg_g*1);
+        gar = ceil(avg_g*7);
+        cout << "Brasil " << bra << " x Alemanha " << gar << endl;
+    }
+    return 0;
+}
