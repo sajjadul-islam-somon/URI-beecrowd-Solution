e8bc071391d8e9d4d520644b62b8a4a7d771a2e1
Sajjadul Islam Shuvo <100591443+sajjadul-islam-shuvo@users.noreply.github.com>
2023-02-10 00:09:52 +0600

    Create 2137.cpp

---
+++ 2137.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    int n;
+    while (cin >> n)
+    {
+        int a[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        sort(a, a + n);
+        for (int i = 0; i < n; i++)
+        {
+            printf("%04d\n", a[i]);
+        }
+    }
+    return 0;
+}
